8808fc190300714a93f860aca43938859aba6747
KarolinaG26 <karolina.m.zajac@gazeta.pl>
2023-12-03 12:58:08 +0100

    3 commit

--- book.py
+++ book.py
@@ -5,6 +5,14 @@ class Book:
         self.year = year
     def get_info(self):
         return f"Ksiazka: {self.title} Autor: {self.author} Rok: {self.year}"
+    def chage_title(self, new_title):
+        self.title = new_title
+    def chage_author(self, new_author):
+        self.author = new_author
+
+    def change_year(self, new_year):
+        self.year = new_year
+
 
 #book = Book("Wiedzmin", "AS", 2000)
 #print(book.get_info())
